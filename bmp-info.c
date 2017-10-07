cddf3855828bcbfbdc3300061a6a8bb3b394aa6e
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-10-07 14:37:11 +0300

    Add
    added extra parts of the code

--- bmp-info.c
+++ bmp-info.c
@@ -1,3 +1,35 @@
+
+#include <stdio.h>
+#include <stdint.h>
+//#include "read.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+
+void *read_image(char *path) {
+        int fd = open(path, O_RDONLY);
+        if (fd < 0) {
+            return NULL;
+        }
+        size_t size = 1000;
+        size_t offset = 0;
+        size_t res;
+        char *buff = malloc(size);
+
+        while((res = read(fd, buff + offset, 100)) != 0) {
+                offset += res;
+                if (offset + 100 > size) {
+                        size *= 2;
+                        buff = realloc(buff, size);
+                }
+        }
+        close(fd);
+        return buff;
+}
+
 void bmp_info(char *);
 
 int main(/*char* argv[]*/)
@@ -15,3 +47,4 @@ void bmp_info(char *file_name)
     height = *(f+22);
     printf("BMP image width: %d\nBMP image height: %d\n", width, height);
 }
+
