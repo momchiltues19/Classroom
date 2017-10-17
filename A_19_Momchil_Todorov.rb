7f4fcd352d436545d3c66719259ebe9ad000efaf
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-10-17 22:15:49 +0300

    Update A_19_Momchil_Todorov.rb

--- A_19_Momchil_Todorov.rb
+++ A_19_Momchil_Todorov.rb
@@ -11,7 +11,7 @@ CSV.foreach(input_file) do |row|
 	r3 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/intervals`
 	r4 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/lin_regressions`
 
-	if r1 == "528"  && r2== "256" && r3 == "525" && r4 == ","
+	if r1 == "528"  && r2== "256" && r3 == "525" && r4 == "1.000000,0.000000"
 		result = 1
 	else 
 		result = 0
@@ -20,4 +20,4 @@ CSV.foreach(input_file) do |row|
 			puts "#{result}\n" 
 	end
 	check+=1
-end
+end 
