ce9f369ff6f6ffd8d0f995b63bb3553eb8db1bc9
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-10-17 21:52:37 +0300

    Update A_19_Momchil_Todorov.rb

--- A_19_Momchil_Todorov.rb
+++ A_19_Momchil_Todorov.rb
@@ -1,23 +1,23 @@
 require 'csv'
 file = ARGV[0]
 
-count = 0
+check = 0
 CSV.foreach(input_file) do |row| 
 	result = 0
-	 = row[5]
+	heroku_url = row[5]
 
-	r1 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/sums` 
-   	r2 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/lin_regressions`  
-	r3 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/filters`
-	r4 = `curl -s -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/intervals`
+	r1 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/sums` 
+   	r2 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/filters`  
+	r3 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/intervals`
+	r4 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/lin_regressions`
 
-	if r1 == result_curl_1  && r2== result_curl_2 && r3 == result_curl_3 && r4 == result_curl_4
+	if r1 == "528"  && r2== "256" && r3 == "525" && r4 == ","
 		result = 1
 	else 
 		result = 0
 	end
-	if count > 0 && !current_url.nil?
-			puts "#{row[1]},#{row[2]},#{row[3]},#{row[4]},#{result}\n" 
+	if check > 0 && !heroku_url.nil?
+			puts "#{result}\n" 
 	end
-	count+=1
+	check+=1
 end
