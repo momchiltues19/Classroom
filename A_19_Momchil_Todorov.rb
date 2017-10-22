a703199dc3541f87b683be8f67efcc96f7aa2134
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-10-22 22:33:13 +0300

    Update A_19_Momchil_Todorov.rb

--- A_19_Momchil_Todorov.rb
+++ A_19_Momchil_Todorov.rb
@@ -1,8 +1,8 @@
 require 'csv'
-file = ARGV[0]
+file = File.new(ARGV[0])
 
 check = 0
-CSV.foreach(input_file) do |row| 
+CSV.foreach(file) do |row| 
 	result = 0
 	heroku_url = row[5]
 
@@ -11,13 +11,13 @@ CSV.foreach(input_file) do |row|
 	r3 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/intervals`
 	r4 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/lin_regressions`
 
-	if r1 == "528"  && r2== "256" && r3 == "525" && r4 == "1.000000,0.000000"
+	if r1 == "528.00"  && r2== "256.00" && r3 == "525.00" && r4 == "1.000000,0.000000"
 		result = 1
 	else 
 		result = 0
 	end
 	if check > 0 && !heroku_url.nil?
-			puts "#{result}\n" 
+			puts "#{row[1]}, #{row[2]} - #{result}\n" 
 	end
 	check+=1
 end 
