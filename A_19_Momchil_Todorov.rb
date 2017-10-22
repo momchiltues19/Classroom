a0d50f6598766ebc6a570ff211615998c1bfd459
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-10-22 23:30:10 +0300

    Update A_19_Momchil_Todorov.rb

--- A_19_Momchil_Todorov.rb
+++ A_19_Momchil_Todorov.rb
@@ -1,23 +1,26 @@
 require 'csv'
 file = File.new(ARGV[0])
+csv = File.new(ARGV[1])
+fixture = CSV.open(csv,'r') { |csv| csv.first}
+csv_path = File.absolute_path(csv)  
 
 check = 0
 CSV.foreach(file) do |row| 
 	result = 0
 	heroku_url = row[5]
 
-	r1 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/sums` 
-   	r2 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/filters`  
-	r3 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/intervals`
-	r4 = `curl -s -F \"file=@./A_19_Momchil_Todorov.csv\" #{heroku_url}/lin_regressions`
+	r1 = `curl -s -F \"file=@.#{csv_path}\" #{heroku_url}/sums` 
+   	r2 = `curl -s -F \"file=@.#{csv_path}\" #{heroku_url}/filters`  
+	r3 = `curl -s -F \"file=@.#{csv_path}\" #{heroku_url}/intervals`
+	r4 = `curl -s -F \"file=@.#{csv_path}\" #{heroku_url}/lin_regressions`
 
-	if r1 == "528.00"  && r2== "256.00" && r3 == "525.00" && r4 == "1.000000,0.000000"
+	if r1 == fixture[0].to_s  && r2== fixture[1].to_s && r3 == fixture[2].to_s && r4 == fixture[3].to_s
 		result = 1
 	else 
 		result = 0
 	end
 	if check > 0 && !heroku_url.nil?
-			puts "#{row[1]}, #{row[2]} - #{result}\n" 
+			puts "#{row[1]},#{row[2]},#{row[3]},#{row[4]},#{result}\n" 
 	end
 	check+=1
 end 
