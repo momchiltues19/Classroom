89f5459da23d6f170db3e175e9cfe7d9a1820bfe
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-10-30 10:46:12 +0200

    Create cat.c

---
+++ os/cat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+	ssize_t wresult = 0;
+	ssize_t rresult = 0;	
+	char buff[100];
+
+	int fd = open("cat.c", O_RDONLY);
+	if(fd < 0)
+	{
+		perror("open");
+		return -1;
+	}
+	
+	while((rresult = read(fd, buff, 100)) != 0)
+	{
+		wresult = 0;
+		while(wresult != rresult)
+		{	
+			ssize_t res;		
+			res = write(STDOUT_FILENO, buff + wresult, rresult - wresult);
+			if (res < 0)
+			{
+				return -1;
+			}
+			wresult += res;		
+		}	
+	}	
+	
+	if (close(fd) < 0)
+	{
+		perror("close");
+		return -1;
+	}
+	return 0;
+}
