667eda28b837803ffb6fb764fc42c24f37ed3f79
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-10-30 10:02:18 +0200

    Update write.c

--- os/write.c
+++ os/write.c
@@ -7,9 +7,17 @@ int main()
 	const char *s = "Hi\n";
 	size_t len = strlen (s);
 	size_t written = 0;
-	ssize_t wresult;
+	ssize_t wresult = 0;
 
-	wresult = write(STDOUT_FILENO, s, len);
-
+	while(wresult != len)
+	{	
+		ssize_t res;		
+		res = write(STDOUT_FILENO, s + wresult, len - wresult);
+		if (res < 0)
+		{
+			return -1;
+		}
+		wresult = res;
+	}	
 	return 0;
 }
