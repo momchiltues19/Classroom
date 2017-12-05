1761bb95c7974db8de2b9e130d316269a5ee64ee
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-12-05 23:24:58 +0200

    Update rome_converter_spec.rb

--- tp/rome_converter_tmp/roman_num/spec/rome_converter_spec.rb
+++ tp/rome_converter_tmp/roman_num/spec/rome_converter_spec.rb
@@ -2,34 +2,34 @@ require "rome_converter"
 
 RSpec.describe RomeConverter do
 	describe "RomeConverter.to_dec and RomeConverter.to_rome"
-		context "when executed =" do
+		context "when executed " do
 			it "converts decimal number to roman" do
 				for i in 1..4000 
-					expect(RomeConverter.to_dec(RomeConverter.to_rome)).to eql i				
+					expect(RomeConverter.to_dec(RomeConverter.to_rome(i)).to eql i				
 				end
 			end
-			it "1231 => MCCXXXI" do
+			it "makes 1231 into MCCXXXI" do
 				expect(RomeConverter.to_rome(1231)).to eql "MCCXXXI"
 			end
-			it "444 => CDXLIV" do
+			it "makes 444 into CDXLIV" do
 				expect(RomeConverter.to_rome(444)).to eql "CDXLIV"
 			end
-			it "567 => DLXVII" do
+			it "makes 567 into DLXVII" do
 				expect(RomeConverter.to_rome(567)).to eql "DLXVII"
 			end
-			it "999 => CMXCIX" do
+			it "makes 999 into CMXCIX" do
 				expect(RomeConverter.to_rome(999)).to eql "CMXCIX"			
 			end	
-			it "MMCCCXII => 2312" do
+			it "makes MMCCCXII into 2312" do
 				expect(RomeConverter.to_dec("MMCCCXII")).to eql 2312
 			end
-			it "CDXCIV => 494" do
+			it "makes CDXCIV into 494" do
 				expect(RomeConverter.to_dec("CDXCIV")).to eql 494
 			end
-			it "DLXXVI => 576" do
+			it "makes DLXXVI into 576" do
 				expect(RomeConverter.to_dec("DLXXVI")).to eql 576
 			end
-			it "CMXLIX => 949" do
+			it "makes CMXLIX into 949" do
 				expect(RomeConverter.to_dec("CMXLIX")).to eql 949			
 			end		
 		end
