52f948366f4cf7cc1a23bdb282c779a28aa5aab9
momchil <momchiltues19>
2018-03-10 20:31:20 +0200

    new

--- os/miners.c
+++ os/miners.c
@@ -4,35 +4,61 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int gold = 0;
+pthread_mutex_t mutex;
 
 void* mine(void* person)
 {
-
+    for(int i = 0; i < 20; i++)
+    {
+        pthread_mutex_lock(&mutex);
+        gold += 10;
+        pthread_mutex_unlock(&mutex);
+        printf("Miner %d gathered 10 gold\n", (int)person);
+        sleep(2);
+    }
 }
 
 void* trade(void* person)
 {
-
+    for(int i = 0; i < 20; i++)
+    {
+        if(gold == 0)
+        {
+            printf("The warehouse is empty, cannot sell!\n");
+        }
+        else 
+        {           
+            pthread_mutex_lock(&mutex);
+            gold -= 10;
+            pthread_mutex_unlock(&mutex);
+        }
+        printf("Trader %d sold 10 gold\n", (int)person);
+        sleep(2);
+    }
 }
 
-int main(int argc, char* argv[])
+int main(/*int argc, char* argv[]*/)
 {
-	//pthread_mutex_init(&mutex, NULL); 	
+	pthread_mutex_init(&mutex, NULL); 	
 	pthread_t threads[100];
 	int i = 0;
-	int miners = atoi(argv[1]);
-	int traders = atoi(argv[2]);
-	do
-	{	
-		if(command[0] == 'p')
-		{
-			pthread_create(&threads[i], NULL, , (void *)N);
-			i++;
-		}			
-	}while(1);
-	//pthread_mutex_destroy();
+    int miners = 1, traders = 1;
+	//int max_miners = 1, max_traders = 1;
+    //if(argc == 3)
+    //{
+    //    max_miners = atoi(argv[1]);
+	//    max_traders = atoi(argv[2]);
+    //}
+
+    pthread_create(&threads[0], NULL, mine, (void *)miners);
+    pthread_create(&threads[1], NULL, trade, (void *)traders);
+
+	pthread_mutex_destroy(&mutex);
+    pthread_join(threads[1], NULL);
+    printf("Gold: %d\n", gold);
 	pthread_exit(NULL);
 	
 }
